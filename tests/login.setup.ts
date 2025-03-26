7c5a9a0109cf06fa00f49f2b4dfd2d5a6e22fb87
Saadat23-4 <saadat.nurbek.ch@gmail.com>
2025-03-25 21:14:26 -0500

    Added EF-67[EF-54 EF-55]

---
+++ tests/login.setup.ts
@@ -0,0 +1,22 @@
+import { test as setup } from "@playwright/test";
+import { Credentials, URLs, screenSize } from "../constants/links";
+
+setup("Login and save session", async ({ page }) => {
+  await page.setViewportSize(screenSize);
+
+  await page.goto(URLs.login);
+
+  await page.getByRole("textbox", { name: "Email" }).fill(Credentials.email);
+
+  await page
+    .getByRole("textbox", { name: "Password" })
+    .fill(Credentials.password);
+  await page.getByRole("button", { name: "Login" }).click();
+
+  await page.waitForURL(
+    (url) =>
+      url.pathname === URLs.onboarding_form || url.pathname === URLs.units
+  );
+
+  await page.context().storageState({ path: ".auth/login.json" });
+});
