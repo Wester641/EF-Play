94e9f9819b4006c410438fe4988829c1cd72ea02
ozodbek <ozodbek200017@gmail.com>
2025-05-16 17:08:46 +0600

    EF-65 EF-66

---
+++ tests/GM/reports_Issues/EF-66/Selectors.ts
@@ -0,0 +1,13 @@
+export const Selectors = {
+  list_of_issuesA:
+    "List of IssuesA comprehensive overview of all issues registered.Issues",
+
+  search_bar: 'input[type="text"]',
+
+  blockText: ".ModalFiltersStaticOptions_blockFiltering__blockText__lZ7eI",
+
+  issues: " Issues by StatusOFF/ONResolved",
+
+  vehicles:
+    "Top 5 Vehicles with Most IssuesOFF/ONTrailer #1000 6Trailer #628 5Trailer #",
+};
