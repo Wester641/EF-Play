a98f97a59dbd7df313a7bb109610ac4bd1af05f8
ozodbek <ozodbek200017@gmail.com>
2025-07-02 15:03:34 +0600

    EF-93 EA-95

--- tests/GM/meter-history/EF-95/Selectors.ts
+++ tests/GM/meter-history/EF-95/Selectors.ts
@@ -1,5 +1,8 @@
 export const Selectors = {
-    perPageDropdown: ".Pagination_paginationCont__range__perPage__option__6SSOl",
-    numbersPerPage: ".css-1km1ehz",
-    dataRow: ".css-1liixou"
+  perPageDropdown: ".Pagination_paginationCont__range__perPage__option__6SSOl",
+  numbersPerPage: ".css-1km1ehz",
+  dataRow: ".css-1liixou",
+
+  urlLimit20: "https://dev-app.easyfleet.ai/meter-history?limit=20&page=1",
+  urlLimit50: "https://dev-app.easyfleet.ai/meter-history?limit=50&page=1",
 };
--- tests/GM/meter-history/EF-95/dropdown_functionality.spec.ts
+++ tests/GM/meter-history/EF-95/dropdown_functionality.spec.ts
@@ -4,59 +4,17 @@ import { URLs, screenSize } from "../../../../constants/links";
 
 test("EF-95__Verify 'Per Page' Dropdown Functionality", async ({ page }) => {
   await page.setViewportSize(screenSize);
-  
+
   await page.goto(URLs.meterHistory);
 
-  await page.waitForTimeout(500);
+  await page.waitForTimeout(5000);
 
-  // Add styling
-  await page.addStyleTag({
-    content: `
-      ${Selectors.dataRow},
-      ${Selectors.perPageDropdown} {
-        background-color: #7d9ec087 !important; 
-        border: 1px solid #7d9ec087 !important;      
-      }`,
-  });
+  await page.locator("button").filter({ hasText: "Per Page10" }).click();
+  await page.getByRole("menuitem", { name: "20" }).click();
+  await expect(page).toHaveURL(Selectors.urlLimit20);
 
-  await page.waitForTimeout(500);
+  await page.locator("button").filter({ hasText: "Per Page20" }).click();
+  await page.getByRole("menuitem", { name: "50" }).click();
 
-  // Remove styling
-  await page.addStyleTag({
-    content: `
-      ${Selectors.dataRow},
-      ${Selectors.perPageDropdown} {
-        background-color: transparent !important;
-        border: none !important;
-      }`,
-  });
-
-  await page.waitForTimeout(3000);
-
-  await expect(page.locator(Selectors.perPageDropdown)).toBeVisible();
-
-  const dataCount10 = await page.locator(Selectors.dataRow).count();
-
-  await expect(dataCount10).toBe(10);
-
-  await page.locator(Selectors.perPageDropdown).click();
-
-  await page.locator(Selectors.numbersPerPage).nth(1).click();
-
-  await page.waitForTimeout(1000);
-
-  const dataCount20 = await page.locator(Selectors.dataRow).count();
-
-  await expect(dataCount20).toBe(20);
-
-  await page.locator(Selectors.perPageDropdown).click();
-
-  await page.locator(Selectors.numbersPerPage).nth(2).click();
-
-  await page.waitForTimeout(1000);
-
-  const dataCount50 = await page.locator(Selectors.dataRow).count();
-
-  await expect(dataCount50).toBeGreaterThan(20);
-  
+  await expect(page).toHaveURL(Selectors.urlLimit50);
 });
