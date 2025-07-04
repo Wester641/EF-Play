aea338168122da0b48ee681bd4e1825f5da8364d
ozodbek <ozodbek200017@gmail.com>
2025-07-04 19:45:40 +0600

    EF-207

--- tests/SM/inspections/inspection-form/EF-207/Selectors.ts
+++ tests/SM/inspections/inspection-form/EF-207/Selectors.ts
@@ -2,5 +2,8 @@ export const Selectors = {
   tabButton: ".css-1q2h7u5",
   dataCell: ".InspectionsFormsList_renderCell__title__te5Zz",
   settingsDropdown: ".css-r8u8y9",
-  pageButton: ".css-13zgp8s"
+  pageButton: ".css-13zgp8s",
+
+  unarchiveButton: "button.Meatballs_button__wNC2-",
+  formsURL: "https://dev-app.easyfleet.ai/inspections/forms/",
 };
