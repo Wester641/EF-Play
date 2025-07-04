4b8d5f7d4e08464d9056953e34dca6636bb59158
zafarzhon <zafarzhon77@gmail.com>
2025-07-04 09:51:53 -0700

    fixed

--- tests/SM/inspections/inspection-form/EF-207/Selectors.ts
+++ tests/SM/inspections/inspection-form/EF-207/Selectors.ts
@@ -3,7 +3,5 @@ export const Selectors = {
   dataCell: ".InspectionsFormsList_renderCell__title__te5Zz",
   settingsDropdown: ".css-r8u8y9",
   pageButton: ".css-13zgp8s",
-
   unarchiveButton: "button.Meatballs_button__wNC2-",
-  formsURL: "https://dev-app.easyfleet.ai/inspections/forms/",
 };
--- tests/SM/inspections/inspection-form/EF-207/unarchive_functionality.spec.ts
+++ tests/SM/inspections/inspection-form/EF-207/unarchive_functionality.spec.ts
@@ -1,30 +1,3 @@
-// import { test, expect } from "@playwright/test";
-// import { Selectors } from "./Selectors";
-// import { screenSize, URLs } from "../../../../../constants/links";
-
-// test("EF-207__Unarchive Forms Functionality", async ({ page }) => {
-//   await page.setViewportSize(screenSize);
-
-//   await page.goto(URLs.inspectionForms);
-
-//   await expect(page).toHaveURL(
-//     "https://dev-app.easyfleet.ai/inspections/forms/"
-//   );
-
-//   await page.getByRole("tab", { name: "Archived" }).click();
-
-//   await page
-//     .locator("tbody tr")
-//     .first()
-//     .locator("td")
-//     .last()
-//     .locator("button.Meatballs_button__wNC2-")
-//     .click();
-//   await page.getByRole("menuitem", { name: "Unarchive" }).click();
-//   await expect(page.getByText("Unarchived")).toBeVisible();
-//   await page.getByRole("tab", { name: "Active" }).click();
-// });
-
 import { test, expect } from "@playwright/test";
 import { Selectors } from "./Selectors";
 import { screenSize, URLs } from "../../../../../constants/links";
@@ -32,9 +5,9 @@ import { screenSize, URLs } from "../../../../../constants/links";
 test("EF-207__Unarchive Forms Functionality", async ({ page }) => {
   await page.setViewportSize(screenSize);
 
-  await page.goto(Selectors.formsURL);
+  await page.goto(URLs.inspectionForms);
 
-  await expect(page).toHaveURL(Selectors.formsURL);
+  await expect(page).toHaveURL(URLs.inspectionForms);
 
   await page.getByRole("tab", { name: "Archived" }).click();
 
