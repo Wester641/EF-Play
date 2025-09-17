c2cdf7b211ded4bf67c4e8cfdb70f8670b7392d2
MaksatI2 <zer0icemax@gmail.com>
2025-09-17 16:59:17 +0600

    EF-128: SVG icon selectors for Papers test have been clarified

--- tests/GM/papers/EF-128/Selectors.ts
+++ tests/GM/papers/EF-128/Selectors.ts
@@ -1,5 +1,5 @@
 export const Selectors = {
-  svg_third_child: "svg:nth-child(3)",
+  svg_third_child: "svg.MuiSvgIcon-colorPrimary[width='21'][height='21']",
   svg_fourth_child: "svg:nth-child(4)",
   click_first_cell: "table.MuiTable-root tr:first-child td:first-child",
   cursor_pointer: ".PaperDetail_cursor_pointer__XdOA7",
