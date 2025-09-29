b563b43be0bba5ce6b458b6cf5e9e66196afea6e
MaksatI2 <zer0icemax@gmail.com>
2025-09-29 09:21:28 +0600

    delete minor selector

--- tests/SM/inspections/inspection-schedules/EF-295/Selectors.ts
+++ tests/SM/inspections/inspection-schedules/EF-295/Selectors.ts
@@ -1,5 +1,3 @@
 export const Selectors = {
   searchInput: ".SearchFiters_containerSearch__searchBox__UwgF2",
-  vehicleNameRow: ".scheduler_default_rowheader_inner_text",
-  vehicleCell: ".scheduler_default_rowheader_inner_text",
 };
